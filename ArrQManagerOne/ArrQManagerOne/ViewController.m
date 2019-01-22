c57ced6b1bb4672b493b8371bd4fc3ec706441cd
ArrQ <2206506738@qq.com>
2019-01-22 13:21:23 +0800

    no message

---
+++ ArrQManagerOne/ArrQManagerOne/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  ArrQManagerOne
+//
+//  Created by admin on 2019/1/22.
+//  Copyright © 2019年 ArrQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+
+
+}
+
+
+@end
---
+++ upload_pod_run.sh
@@ -0,0 +1,28 @@
+# 修改 pod name (2,5,9,27)
+a=`grep -E 's.version.*=' YYTooling.podspec`
+b=${a#*\'}
+version=${b%\'*}
+LineNumber=`grep -nE 's.version.*=' YYTooling.podspec | cut -d : -f1`
+echo "current version is ${version}, please enter the new version:"
+read newVersion
+# 修改YYTooling.podspec文件中的version为指定值
+sed -i "" "${LineNumber}s/${version}/${newVersion}/g" YYTooling.podspec
+# 修改readme版本号
+sed -i "" "s/${version}/${newVersion}/g" README.md
+echo "git commit and git push origin master ？ (y/n):"
+read isCommit
+if [[ ${isCommit} = "y" ]] || [[ ${isCommit} = "Y" ]]; then
+	git add .
+	echo "please ender commit info:"
+	read commitInfo
+	# 先commit再pull后push
+	git commit -am ${commitInfo}
+	echo "push origin master..."
+	git pull origin master
+#    git push origin master
+fi
+echo "add tag and push tag..."
+git tag ${newVersion}
+git push origin master --tags
+pod spec lint YYTooling.podspec --allow-warnings --use-libraries
+pod trunk push YYTooling.podspec --allow-warnings --use-libraries
